9614cb009c3f350343e0d6e985aed9a40c113b2d
James Wu <jameswu144@gmail.com>
2023-08-23 17:28:12 +0100

    highlight selected keys
    23 Aug 23
    
    Keys will now highlight when you select them and revert to original colour when you deselect them.

---
+++ Assets/Scripts/Colour.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class Colour
+{
+    public static Color White = new Color(1, 1, 1);
+    public static Color Black = new Color(0, 0, 0);
+    public static Color LightGrey = new Color(0.7f, 0.7f, 0.7f);
+    public static Color DarkGrey = new Color(0.3f, 0.3f, 0.3f);
+
+}
--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-
+    
     public int octave;
     public int note;
+    private bool isWhiteKey;
+    private Color originalColour;
+    private Color highlightColour;
     public SpriteRenderer sr;
 
-    public void Initialise(int octave, int note)
-    {
-        this.octave = octave;
-        this.note = note;
-    }
+    private bool selected;
 
+    private void Start()
+    {
+        isWhiteKey = Note.IsWhiteKey(note);
+        originalColour = isWhiteKey ? Colour.White : Colour.Black;
+        highlightColour = isWhiteKey ? Colour.LightGrey : Colour.DarkGrey;
+        sr = isWhiteKey ? gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>() : gameObject.GetComponent<SpriteRenderer>();
+        selected = false;
+    }
 
     private void OnMouseDown()
     {
+        sr.color = selected ? originalColour : highlightColour;
+        selected = !selected;
+
         Debug.Log(this);
     }
 
